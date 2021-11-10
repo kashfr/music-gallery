7938e2f50cfb194e68cafa5b4bddf5492bdade15
Kaheem Freeman <kfreeman@Kaheems-MacBook-Pro-2.local>
2021-11-10 17:04:34 -0500

    got the api working

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-// Background Overlay
-// function on() {
-//   document.getElementById("overlay").style.display = "block";
-// }
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
+
+// function off() {
+//   document.getElementById("overlay").style.display = "none";
+// }
+const domain = `https://www.theaudiodb.com/`;
+const apiKey = 523532;
+const TADB_Artist_ID = 112024;
+
+const artistData = `https://www.theaudiodb.com/api/v1/json/${apiKey}/artist.php?i=${TADB_Artist_ID}`
+const artist = "kanye_west";
+console.log(artistData)
+
+
+
+// const artwork = await axios.get;
 
 async function fetchData() {
   // Write code here.
   try {
-    const response = `http://musicbrainz.org/ws/2/artist/?query=artist:sting%20AND%20type:group%20AND%20country:US&fmt=json`;
+    const response = `https://www.theaudiodb.com/api/v1/json/523532/searchalbum.php?s=${artist}`;
+    const res = await axios.get(response);
+    const artistData = res.data;
+    console.log(res.data.strAlbumThumb)
+    console.log(artistData)
+  } catch (error) {
+    console.log(error);
+    alert(`${artist} artist not found`);
+  }
+}
+fetchData()
+
+async function fetchArtistData() {
+  // Write code here.
+  try {
+    const response = `https://www.theaudiodb.com/api/v1/json/523532/artist.php?i=112024`;
     const res = await axios.get(response);
     const artistData = res.data;
     console.log(artistData)
@@ -19,4 +42,9 @@ async function fetchData() {
     alert(`${artist} artist not found`);
   }
 }
-fetchData()
+fetchArtistData()
+
+// https://www.last.fm/music/Drake/Certified+Lover+Boy
+
+// https://www.theaudiodb.com/api/v1/json/523532/searchalbum.php?s=2pac
+// https://www.theaudiodb.com/api/v1/json/523532/searchalbum.php?s=kanye_west
