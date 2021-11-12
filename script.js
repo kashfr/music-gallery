f5c0e5542208e083ce5ed50dd362b5e9172dfa3f
Kaheem Freeman <kfreeman@Kaheems-MacBook-Pro-2.local>
2021-11-12 15:45:29 -0500

    convo with my bro, ron

--- script.js
+++ script.js
@@ -3,29 +3,41 @@
 // }
 const domain = `https://www.theaudiodb.com/`;
 const apiKey = 523532;
-let TADB_Artist_ID = 112024;
+let TADB_Artist_ID = 112059;
 
-let artistData = `https://www.theaudiodb.com/api/v1/json/${apiKey}/artist.php?i=${TADB_Artist_ID}`;
-let artist = "kanye_west";
+// let artistData = `https://www.theaudiodb.com/api/v1/json/${apiKey}/artist.php?i=${TADB_Artist_ID}`;
+// let artist = "kanye";
 let backgroundImage = document.querySelector(".background-image");
 let albumInfo = document.querySelector(".artwork-info");
-// document.getElementById("test").innerHTML = "Ye Season";
+// let artistPic = document.querySelector("#artist-pic");
+
+const test = document.querySelector("#test");
+// test.innerHTML = "Ye Season";
+// document.getElementById("test").innerHTML = "Ye";
 
 async function fetchAlbumData() {
   // Write code here.
   try {
-    const response = `https://www.theaudiodb.com/api/v1/json/${apiKey}/searchalbum.php?s=${artist}`;
+    const response = `https://www.theaudiodb.com/api/v1/json/${apiKey}/searchalbum.php?s=${TADB_Artist_ID}`;
     const res = await axios.get(response);
     const albumData = res.data;
-    let description = albumData.album[0].strDescriptionEN;
+    // let description = albumData.album[0].strDescriptionEN;
     let artwork = albumData.album[0].strAlbumThumb;
     let idArtist = albumData.album[0].idArtist;
-    console.log(idArtist);
-    // backgroundImage.innerHTML = albumData.album[0].strAlbumThumb;
+    document.body.style.backgroundImage = `url("${artwork}")`;
+    // albumInfo.innerHTML = "";
+    let newArtistInfo = document.createElement("p");
+    newArtistInfo.textContent = `${description}`;
+    // test.appendChild(newArtistInfo);
+
+    console.log(artwork);
+    // console.log(idArtist);
+
     // const album = res.data.album.strAlbumThumb;
     console.log(albumData);
-    console.log(albumData.album[0].strAlbumThumb);
+    // console.log(albumData.album[0].strAlbumThumb);
     // console.log(albumData.album[0].strDescriptionEN);
+    fetchArtistData(idArtist);
     console.log(description);
   } catch (error) {
     console.log(error);
@@ -34,19 +46,24 @@ async function fetchAlbumData() {
 }
 fetchAlbumData();
 
-async function fetchArtistData() {
+// function test() {}
+
+async function fetchArtistData(res) {
   // Write code here.
   try {
-    const response = `https://www.theaudiodb.com/api/v1/json/523532/artist.php?i=111822`;
+    const response = `https://www.theaudiodb.com/api/v1/json/${apiKey}/artist.php?i=${res}`;
     const res = await axios.get(response);
-    const artistData = res.data;
-    console.log(artistData);
+    const artistData = res.data.artists[0];
+    // artistPic.src = `${artistData.strArtistThumb}`;
+    document.getElementById("artist-pic").src = `${artistData.strArtistThumb}`;
+
+    console.log(artistData.strArtistThumb);
   } catch (error) {
     console.log(error);
     alert(`${artist} artist not found`);
   }
 }
-fetchArtistData();
+// fetchArtistData();
 
 // https://www.last.fm/music/Drake/Certified+Lover+Boy
 
